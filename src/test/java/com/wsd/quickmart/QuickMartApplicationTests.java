22731173e8131226d6693ff4f184aa95f3d6182a
kakan <kakanghosh69@gmail.com>
2024-06-11 19:20:27 +0600

    Initial commit - project setup

---
+++ src/test/java/com/wsd/quickmart/QuickMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsd.quickmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
