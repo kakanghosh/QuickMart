fd13e66b86267c4de82492f80ff7221649018165
kakan <kakanghosh69@gmail.com>
2024-06-12 12:06:03 +0600

    added for wishlist of user.

--- src/test/java/com/wsd/quickmart/repository/WishListRepositoryTest.java
+++ src/test/java/com/wsd/quickmart/repository/WishListRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.wsd.quickmart.repository;
 
+import com.wsd.quickmart.entity.WishList;
+import com.wsd.quickmart.entity.WishListItem;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -9,6 +11,9 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
@@ -22,16 +27,10 @@ class WishListRepositoryTest {
             .withPassword("a1643af38034caaa687ae9e12f68dc6d");
 
     @Autowired
-    private CustomerRepository customerRepository;
-
-    @Autowired
-    private ProductRepository productRepository;
+    private WishListRepository wishListRepository;
 
     @Autowired
     private WishListItemRepository wishListItemRepository;
-
-    @Autowired
-    private WishListRepository wishListRepository;
 
 
     @DynamicPropertySource
@@ -44,7 +43,6 @@ class WishListRepositoryTest {
     @BeforeAll
     static void beforeAll() {
         mySQLContainer.start();
-
     }
 
     @AfterAll
@@ -60,5 +58,19 @@ class WishListRepositoryTest {
 
     @Test
     void shouldFindWishListByCustomerId() {
+        wishListRepository.saveAll(List.of(
+                new WishList(1L, LocalDateTime.now().minusDays(1)),
+                new WishList(2L, LocalDateTime.now().minusHours(5))
+        ));
+        wishListItemRepository.saveAll(List.of(
+                new WishListItem(1L, 1L),
+                new WishListItem(1L, 2L),
+                new WishListItem(2L, 2L),
+                new WishListItem(2L, 3L)
+        ));
+        var wishListOfUser1 = wishListRepository.findWishListByCustomerId(1L);
+        var wishListOfUser2 = wishListRepository.findWishListByCustomerId(2L);
+        assertThat(wishListOfUser1).isNotEmpty();
+        assertThat(wishListOfUser2).isNotEmpty();
     }
 }
