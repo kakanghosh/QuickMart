2b61cc94ddba6e4eb92b26546fe4e33bad5f710c
kakan <kakanghosh69@gmail.com>
2024-06-13 02:40:37 +0600

    write error logs from advice.

--- src/main/java/com/wsd/quickmart/exception/RestControllerAdvice.java
+++ src/main/java/com/wsd/quickmart/exception/RestControllerAdvice.java
@@ -1,5 +1,7 @@
 package com.wsd.quickmart.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,25 +13,36 @@ import java.time.format.DateTimeParseException;
 
 @ControllerAdvice(annotations = RestController.class)
 public class RestControllerAdvice {
+    private static final Logger logger = LoggerFactory.getLogger(RestControllerAdvice.class);
 
     @ExceptionHandler(InvalidDateRangeException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorResponse> handleInvalidDateRangeException(InvalidDateRangeException exception) {
+        var errorResponse = new ErrorResponse(exception.getMessage(), "InvalidDateRangeException");
+        writeToLog(errorResponse);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                             .body(new ErrorResponse(exception.getMessage(), "InvalidDateRangeException"));
+                             .body(errorResponse);
     }
 
     @ExceptionHandler(DateTimeParseException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorResponse> handleDateTimeParseException(DateTimeParseException exception) {
+        var errorResponse = new ErrorResponse(exception.getMessage(), "DateTimeParseException");
+        writeToLog(errorResponse);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                             .body(new ErrorResponse(exception.getMessage(), "DateTimeParseException"));
+                             .body(errorResponse);
     }
 
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException exception) {
+        var errorResponse = new ErrorResponse(exception.getMessage(), "INTERNAL_SERVER_ERROR");
+        writeToLog(errorResponse);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                             .body(new ErrorResponse(exception.getMessage(), "INTERNAL_SERVER_ERROR"));
+                             .body(errorResponse);
+    }
+
+    private void writeToLog(ErrorResponse errorResponse) {
+        logger.error("Error: {}", errorResponse);
     }
 }
